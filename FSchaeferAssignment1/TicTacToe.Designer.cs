5b406132aa7b13783782eb59c4604222bff48b04
faolanschaefer <48422672+faolanschaefer@users.noreply.github.com>
2023-09-28 19:47:31 -0400

    Add project files.

---
+++ FSchaeferAssignment1/TicTacToe.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace FSchaeferAssignment1
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picA1 = new System.Windows.Forms.PictureBox();
+            this.picA2 = new System.Windows.Forms.PictureBox();
+            this.picA3 = new System.Windows.Forms.PictureBox();
+            this.picB1 = new System.Windows.Forms.PictureBox();
+            this.picB2 = new System.Windows.Forms.PictureBox();
+            this.picB3 = new System.Windows.Forms.PictureBox();
+            this.picC1 = new System.Windows.Forms.PictureBox();
+            this.picC2 = new System.Windows.Forms.PictureBox();
+            this.picC3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picActivePlayer = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.grpGrid = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picActivePlayer)).BeginInit();
+            this.grpGrid.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picA1
+            // 
+            this.picA1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picA1.Location = new System.Drawing.Point(5, 18);
+            this.picA1.Margin = new System.Windows.Forms.Padding(2);
+            this.picA1.Name = "picA1";
+            this.picA1.Size = new System.Drawing.Size(76, 82);
+            this.picA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA1.TabIndex = 0;
+            this.picA1.TabStop = false;
+            this.picA1.Click += new System.EventHandler(this.picA1_Click);
+            // 
+            // picA2
+            // 
+            this.picA2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picA2.Location = new System.Drawing.Point(85, 18);
+            this.picA2.Margin = new System.Windows.Forms.Padding(2);
+            this.picA2.Name = "picA2";
+            this.picA2.Size = new System.Drawing.Size(76, 82);
+            this.picA2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA2.TabIndex = 0;
+            this.picA2.TabStop = false;
+            this.picA2.Click += new System.EventHandler(this.picA2_Click);
+            // 
+            // picA3
+            // 
+            this.picA3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picA3.Location = new System.Drawing.Point(165, 18);
+            this.picA3.Margin = new System.Windows.Forms.Padding(2);
+            this.picA3.Name = "picA3";
+            this.picA3.Size = new System.Drawing.Size(76, 82);
+            this.picA3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picA3.TabIndex = 0;
+            this.picA3.TabStop = false;
+            this.picA3.Click += new System.EventHandler(this.picA3_Click);
+            // 
+            // picB1
+            // 
+            this.picB1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picB1.Location = new System.Drawing.Point(5, 104);
+            this.picB1.Margin = new System.Windows.Forms.Padding(2);
+            this.picB1.Name = "picB1";
+            this.picB1.Size = new System.Drawing.Size(76, 82);
+            this.picB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB1.TabIndex = 0;
+            this.picB1.TabStop = false;
+            this.picB1.Click += new System.EventHandler(this.picB1_Click);
+            // 
+            // picB2
+            // 
+            this.picB2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picB2.Location = new System.Drawing.Point(85, 104);
+            this.picB2.Margin = new System.Windows.Forms.Padding(2);
+            this.picB2.Name = "picB2";
+            this.picB2.Size = new System.Drawing.Size(76, 82);
+            this.picB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB2.TabIndex = 0;
+            this.picB2.TabStop = false;
+            this.picB2.Click += new System.EventHandler(this.picB2_Click);
+            // 
+            // picB3
+            // 
+            this.picB3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picB3.Location = new System.Drawing.Point(165, 104);
+            this.picB3.Margin = new System.Windows.Forms.Padding(2);
+            this.picB3.Name = "picB3";
+            this.picB3.Size = new System.Drawing.Size(76, 82);
+            this.picB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picB3.TabIndex = 0;
+            this.picB3.TabStop = false;
+            this.picB3.Click += new System.EventHandler(this.picB3_Click);
+            // 
+            // picC1
+            // 
+            this.picC1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picC1.Location = new System.Drawing.Point(5, 190);
+            this.picC1.Margin = new System.Windows.Forms.Padding(2);
+            this.picC1.Name = "picC1";
+            this.picC1.Size = new System.Drawing.Size(76, 82);
+            this.picC1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC1.TabIndex = 0;
+            this.picC1.TabStop = false;
+            this.picC1.Click += new System.EventHandler(this.picC1_Click);
+            // 
+            // picC2
+            // 
+            this.picC2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picC2.Location = new System.Drawing.Point(85, 190);
+            this.picC2.Margin = new System.Windows.Forms.Padding(2);
+            this.picC2.Name = "picC2";
+            this.picC2.Size = new System.Drawing.Size(76, 82);
+            this.picC2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC2.TabIndex = 0;
+            this.picC2.TabStop = false;
+            this.picC2.Click += new System.EventHandler(this.picC2_Click);
+            // 
+            // picC3
+            // 
+            this.picC3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picC3.Location = new System.Drawing.Point(165, 190);
+            this.picC3.Margin = new System.Windows.Forms.Padding(2);
+            this.picC3.Name = "picC3";
+            this.picC3.Size = new System.Drawing.Size(76, 82);
+            this.picC3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picC3.TabIndex = 0;
+            this.picC3.TabStop = false;
+            this.picC3.Click += new System.EventHandler(this.picC3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Justice", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(301, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player:";
+            // 
+            // picActivePlayer
+            // 
+            this.picActivePlayer.Location = new System.Drawing.Point(309, 171);
+            this.picActivePlayer.Margin = new System.Windows.Forms.Padding(2);
+            this.picActivePlayer.Name = "picActivePlayer";
+            this.picActivePlayer.Size = new System.Drawing.Size(76, 82);
+            this.picActivePlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picActivePlayer.TabIndex = 0;
+            this.picActivePlayer.TabStop = false;
+            this.picActivePlayer.Click += new System.EventHandler(this.picC3_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Font = new System.Drawing.Font("Justice", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(23, 21);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(375, 69);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grpGrid
+            // 
+            this.grpGrid.Controls.Add(this.picA1);
+            this.grpGrid.Controls.Add(this.picA2);
+            this.grpGrid.Controls.Add(this.picB1);
+            this.grpGrid.Controls.Add(this.picB2);
+            this.grpGrid.Controls.Add(this.picC3);
+            this.grpGrid.Controls.Add(this.picC1);
+            this.grpGrid.Controls.Add(this.picB3);
+            this.grpGrid.Controls.Add(this.picC2);
+            this.grpGrid.Controls.Add(this.picA3);
+            this.grpGrid.Location = new System.Drawing.Point(23, 113);
+            this.grpGrid.Name = "grpGrid";
+            this.grpGrid.Size = new System.Drawing.Size(250, 280);
+            this.grpGrid.TabIndex = 3;
+            this.grpGrid.TabStop = false;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 427);
+            this.Controls.Add(this.grpGrid);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picActivePlayer);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picA3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picActivePlayer)).EndInit();
+            this.grpGrid.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picA1;
+        private System.Windows.Forms.PictureBox picA2;
+        private System.Windows.Forms.PictureBox picA3;
+        private System.Windows.Forms.PictureBox picB1;
+        private System.Windows.Forms.PictureBox picB2;
+        private System.Windows.Forms.PictureBox picB3;
+        private System.Windows.Forms.PictureBox picC1;
+        private System.Windows.Forms.PictureBox picC2;
+        private System.Windows.Forms.PictureBox picC3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picActivePlayer;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.GroupBox grpGrid;
+    }
+}
+
---
+++ FSchaeferAssignment1/TicTacToe.cs
@@ -0,0 +1,236 @@
+﻿/* 
+ * Programmed by: Faolan Schaefer
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSchaeferAssignment1
+{
+    public partial class TicTacToe : Form
+    {
+        private const int SIZE_OF_GRID = 3;
+        private Image xImg = Properties.Resources.x;
+        private Image oImg = Properties.Resources.o;
+        private Player activePlayer = Player.x;
+        private Player[,] grid = new Player[SIZE_OF_GRID, SIZE_OF_GRID];
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Represents one of the possible players
+        /// </summary>
+        enum Player
+        {
+            empty, x, o
+        }
+
+        /// <summary>
+        /// Displays a message to the lblMessage label
+        /// </summary>
+        /// <param name="message">Message to display</param>
+        /// <param name="color">Forecolor of message</param>
+        private void SendMessage(string message, Color color)
+        {
+            lblMessage.ForeColor = color;
+            lblMessage.Text = message;
+        }
+
+        /// <summary>
+        /// Places the active player's token on the selected square
+        /// </summary>
+        /// <param name="location">The PictureBox in the selected location</param>
+        private void PlaceToken(PictureBox location)
+        {
+            SendMessage("", Color.Transparent);
+            if (location.Image == null)
+            {
+                // Place token to visible grid
+                if (activePlayer == Player.x)
+                {
+                    picActivePlayer.Image = oImg;
+                    location.Image = xImg;
+                }
+                else
+                {
+                    picActivePlayer.Image = xImg;
+                    location.Image = oImg;
+                }
+
+                // Update grid array to reflect the visible grid
+                int col = int.Parse(location.Name.Substring(4, 1)) - 1;
+                int row;
+                switch (location.Name.Substring(3, 1))
+                {
+                    case "A":
+                        row = 0;
+                        break;
+                    case "B":
+                        row = 1;
+                        break;
+                    case "C":
+                        row = 2;
+                        break;
+                    default:
+                        row = -1;
+                        break;
+                }
+                grid[row, col] = activePlayer;
+
+                // If win or tie, display a message. Otherwise swap players and continue.
+                if (CheckForWin())
+                {
+                    SendMessage($"Player {activePlayer.ToString().ToUpper()} wins!", Color.Green);
+                    ResetGame();
+                    return;
+                }
+                else if (GridIsFull())
+                {
+                    SendMessage($"It's a tie!", Color.Gray);
+                    ResetGame();
+                    return;
+                }
+                else
+                {
+                    if (activePlayer == Player.x)
+                    {
+                        activePlayer = Player.o;
+                    }
+                    else
+                    {
+                        activePlayer = Player.x;
+                    }
+                }
+            }
+            else
+            {
+                SendMessage("Location is not available", Color.Crimson);
+            }
+        }
+
+        /// <summary>
+        /// Checks all possible win conditions for the active player
+        /// </summary>
+        /// <returns>Whether or not the active player has won</returns>
+        private bool CheckForWin()
+        {
+            for (int i = 0; i < SIZE_OF_GRID; i++)
+            {
+                if (grid[i, 0] == activePlayer && grid[i, 1] == activePlayer && grid[i, 2] == activePlayer)
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < SIZE_OF_GRID; i++)
+            {
+                if (grid[0, i] == activePlayer && grid[1, i] == activePlayer && grid[2, i] == activePlayer)
+                {
+                    return true;
+                }
+            }
+            if (grid[1, 1] == activePlayer)
+            {
+                if (grid[0, 0] == activePlayer && grid[2, 2] == activePlayer)
+                {
+                    return true;
+                }
+                else if (grid[0, 2] == activePlayer && grid[2, 0] == activePlayer)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if the visible grid is full
+        /// </summary>
+        /// <returns>Whether or not the grid is full</returns>
+        private bool GridIsFull()
+        {
+            bool gridIsFull = true;
+            foreach (PictureBox item in grpGrid.Controls)
+            {
+                if (item.Image == null)
+                {
+                    gridIsFull = false;
+                }
+            }
+            return gridIsFull;
+        }
+
+        /// <summary>
+        /// Resets the game to its inital state
+        /// </summary>
+        private void ResetGame()
+        {
+            foreach (PictureBox item in grpGrid.Controls)
+            {
+                item.Image = null;
+            }
+            grid = new Player[SIZE_OF_GRID, SIZE_OF_GRID];
+            activePlayer = Player.x;
+            picActivePlayer.Image = xImg;
+        }
+
+        private void picA1_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picA1);
+        }
+
+        private void picA2_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picA2);
+        }
+
+        private void picA3_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picA3);
+        }
+
+        private void picB1_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picB1);
+        }
+
+        private void picB2_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picB2);
+        }
+
+        private void picB3_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picB3);
+        }
+
+        private void picC1_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picC1);
+        }
+
+        private void picC2_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picC2);
+        }
+
+        private void picC3_Click(object sender, EventArgs e)
+        {
+            PlaceToken(picC3);
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            picActivePlayer.Image = xImg;
+        }
+    }
+}
